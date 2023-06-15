f4ea65e8ba644318eef71c15785397e17b6e8d9c
TogoFire <togofire@mailfence.com>
2023-06-15 11:31:41 -0300

    dubai: Add support for ViPER4Android FX Redesign
    Viper's Audio is software for various platforms, such as Android and PC, Cat.
    It is used to improve the sound in general, with the help of various sound effects,
    i.e. it is actually a system equalizer.
    
    Reference:
    https://github.com/TogoFire/packages_apps_ViPER4AndroidFX
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Akshat <Legendleo90@users.noreply.github.com>
    Change-Id: I4cdc7d278d01d91bb68048ea71850d049336af2b

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,3 +18,7 @@ git clone https://github.com/Dubai-Resources/android_kernel_motorola_sm7325.git 
 # Hardware
 rm -rf hardware/motorola
 git clone https://github.com/LineageOS/android_hardware_motorola.git -b lineage-22.1 hardware/motorola
+
+# Viper4FX
+rm -rf packages/apps/ViPER4AndroidFX
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
