e254397533a68beeb80050d02aea9e0dacf5dd86
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2025-01-17 19:48:49 +0530

    dubai: vendorsetup: Clone KSU Next branch of Kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,24 @@
-@ -0,0 +1,32 @@
 # Common Device Tree
 rm -rf device/motorola/sm7325-common
-git clone https://github.com/Dubai-Resources/android_device_motorola_sm7325-common.git -b lineage-22.1 device/motorola/sm7325-common --depth=1
+git clone https://github.com/Dubai-Resources/android_device_motorola_sm7325-common.git -b 15.0 device/motorola/sm7325-common --depth=1
 
 # Vendor Tree
 rm -rf vendor/motorola/dubai
-git clone https://github.com/Dubai-Resources/proprietary_vendor_motorola_dubai.git -b lineage-22.1 vendor/motorola/dubai --depth=1
+git clone https://github.com/Dubai-Resources/proprietary_vendor_motorola_dubai.git -b lineage-22.2 vendor/motorola/dubai --depth=1
 
 # Common Vendor Tree
 rm -rf vendor/motorola/sm7325-common
-git clone https://github.com/Dubai-Resources/proprietary_vendor_motorola_sm7325-common.git -b lineage-22.1 vendor/motorola/sm7325-common --depth=1
+git clone https://github.com/Dubai-Resources/proprietary_vendor_motorola_sm7325-common.git -b lineage-22.2 vendor/motorola/sm7325-common --depth=1
 
 # Kernel Tree
 rm -rf kernel/motorola/sm7325
-git clone https://github.com/Dubai-Resources/android_kernel_motorola_sm7325.git -b lineage-22.1 kernel/motorola/sm7325 --depth=1
+git clone https://github.com/Rakhshan7070/android_kernel_motorola_sm7325.git -b 15.0-KSU kernel/motorola/sm7325 --depth=1
 
 # Hardware
 rm -rf hardware/motorola
-git clone https://github.com/RisingTechOSS-devices/hardware_motorola.git -b fifteen hardware/motorola
+git clone https://github.com/RisingOS-Revived-devices/hardware_motorola.git -b fifteen hardware/motorola
+
+# Setup KSU-Next
+cd kernel/motorola/sm7325
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
+cd ../../..
