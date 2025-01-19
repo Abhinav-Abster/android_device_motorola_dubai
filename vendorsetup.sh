80d5d5c122dd0129da85bc5f32456d5891054a64
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2025-01-19 13:51:15 +0530

    dubai: Use ViPER4AndroidFX from hardware/motorola
    * Use ViPER4AndroidFX from hardware_motorola repo.
    
    Also remove additional cloning of ViPER4AndroidFX
    
    Signed-off-by: Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,8 +17,4 @@ git clone https://github.com/Dubai-Resources/android_kernel_motorola_sm7325.git 
 
 # Hardware
 rm -rf hardware/motorola
-git clone https://github.com/LineageOS/android_hardware_motorola.git -b lineage-22.1 hardware/motorola
-
-# Viper4FX
-rm -rf packages/apps/ViPER4AndroidFX
-git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
+git clone https://github.com/RisingTechOSS-devices/hardware_motorola.git -b fifteen hardware/motorola
