9c377ba5b8d64f470f29cafaf02f84397c5c4fac
Dheeraj R Shrivatsa <Dheeraj3031A@users.noreply.github.com>
2025-10-11 18:47:26 +0530

    dubai: vendorsetup: Track 16.0 branch for kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,7 +12,7 @@ git clone https://github.com/Dubai-Resources/proprietary_vendor_motorola_sm7325-
 
 # Kernel Tree
 rm -rf kernel/motorola/sm7325
-git clone https://github.com/Dubai-Resources/android_kernel_motorola_sm7325.git -b 15.0 kernel/motorola/sm7325 --depth=1
+git clone https://github.com/Dubai-Resources/android_kernel_motorola_sm7325.git -b 16.0 kernel/motorola/sm7325 --depth=1
 
 # Hardware
 rm -rf hardware/motorola
