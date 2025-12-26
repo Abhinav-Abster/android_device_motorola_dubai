b552bc758e3a196f2cb0c5844088e42264a4f26f
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-12-26 14:46:52 +0000

    dubai: Use sensors HAL from hardware/motorola
    Change-Id: I90a3dd06c3f949ee14e434baf859bd6d1a874729

--- device.mk
+++ device.mk
@@ -88,13 +88,6 @@ PRODUCT_COPY_FILES += \
 # Priv-Keys
 -include vendor/infinity-priv/keys/keys.mk
 
-# Sensors
-PRODUCT_PACKAGES += \
-    sensors.dubai
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackages
