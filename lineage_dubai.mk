445ea1ef9aa8e6ef832070a762f1c97717005e45
Abhinav-Abster <abhinavricky02@gmail.com>
2026-05-08 13:37:49 +0530

    optimized for Lunaris

--- lineage_dubai.mk
+++ lineage_dubai.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from dubai device
 $(call inherit-product, device/motorola/dubai/device.mk)
 
-# Inherit some common Infinity-X stuff.
-#$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_dubai
 PRODUCT_DEVICE := dubai
@@ -27,21 +27,18 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=motorola/dubai_g/dubai:14/U1RDS34.80-40-5-6/ba4fd-68ef4:user/release-keys \
     DeviceProduct=dubai_g
 
-# Infinity-X stuff
-#INFINITY_BUILD_TYPE := OFFICIAL
-#INFINITY_MAINTAINER := NotDheeraj06
-#TARGET_BOOT_ANIMATION_RES := 1080
-#TARGET_SUPPORTS_BLUR := true
-#TARGET_HAS_UDFPS := true
-#WITH_GAPPS := true
-#TARGET_SUPPORTS_QUICK_TAP := true
-#TARGET_FACE_UNLOCK_SUPPORTED := true
+# Lunaris stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
-#Lunaris Stuff
 TARGET_SUPPORTED_REFRESH_RATES := 60,90,120,144
 TARGET_CUSTOM_UDFPS := true
 WITH_GMS := true
 WITH_BCR := true
+TARGET_SUPPORTS_GOOGLE_TELEPHONY := true
+USE_REALITY_ENGINE := true
 
 # Perf Activity Anim Override
 PERF_ANIM_OVERRIDE := true
